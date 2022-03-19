171334f0b02a2ebc47576a90586c49e54b3954bb
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-03-19 15:52:18 +0500

    Getting data from database

--- commands/start.js
+++ commands/start.js
@@ -1,26 +1,24 @@
 const { config } = require('../config');
+const User = require('../models/user.model');
 const { isAdmin, isRegistered } = require('../utils/');
 const start = async (ctx) => {
-  const user = ctx.from;
   (await isRegistered(ctx))
     ? await ctx.replyWithHTML('<b>🔥 You have already registered</b> ')
     : await ctx.replyWithHTML('<b>🙅‍♂️ You are not registered</b>');
 
   (await isAdmin(ctx)) && ctx.replyWithHTML(`<b>🔥 You are admin!</b>`);
-
+  const user = await User.findOne({ user_id: ctx.from.id });
   await ctx.replyWithHTML(
-    `<b>👋 Hello, ${ctx.from.first_name}${
-      user.last_name ? ' ' + user.last_name : ''
-    }!</b>` +
+    `<b>👋 Hello, ${user.first_name} ${user.last_name}!</b>` +
       `\n` +
-      `<b>👨‍💻 Your current id:</b> ${user.id}` +
+      `<b>👨‍💻 Your current id:</b> ${user.user_id}` +
       `\n` +
-      `<b>💰 Your current ballance:</b> ${0} sums` +
+      `<b>💰 Your current balance:</b> ${user.balance} sums` +
       `\n\n` +
       `📝 <b>Invite your friends to our application and earn balls:</b>` +
       `\n` +
       `Share this link: https://t.me/` +
-      `${config.BOT_USERNAME}/start?id=${user.id}`
+      `${config.BOT_USERNAME}/start?id=${user._id}`
   );
 };
 
